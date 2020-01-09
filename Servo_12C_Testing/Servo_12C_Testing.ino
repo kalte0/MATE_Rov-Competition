25e5d34bef67f6dbf53b4ad3c9b55b768bf52bc7
Renata Del Vecchio <renata@cyren.org>
2020-01-09 08:47:55 -0500

    worked on making 12 channel motor controller work

--- Servo_12C_Testing/Servo_12C_Testing.ino
+++ Servo_12C_Testing/Servo_12C_Testing.ino
@@ -1,11 +1,32 @@
-#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 60 // Analog servos run at ~60 Hz updates
+
+uint8_t servonum = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
 
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz
+  pwm.setPWMFreq(SERVO_FREQ);
+
+pwm.writeMicroseconds(0, 1500); 
+   delay(10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  pwm.writeMicroseconds(0, 1500); 
+  delay(3000); 
+  pwm.writeMicroseconds(0 ,1900); 
+  delay(3000); 
 }
+
