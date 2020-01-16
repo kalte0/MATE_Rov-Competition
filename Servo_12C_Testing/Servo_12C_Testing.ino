872e7663dcacdadb1102c8a914c667f6e70c0829
Renata Del Vecchio <renata@cyren.org>
2020-01-16 08:49:01 -0500

    Got a servo controlled by the 12c motor controller, still working on the full motor.

--- Servo_12C_Testing/Servo_12C_Testing.ino
+++ Servo_12C_Testing/Servo_12C_Testing.ino
@@ -3,10 +3,10 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVOMIN  300 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 450  // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  1200 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  1800 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 60 // Analog servos run at ~60 Hz updates
 
 uint8_t servonum = 0;
@@ -20,13 +20,13 @@ void setup() {
   pwm.setPWMFreq(SERVO_FREQ);
 
 pwm.writeMicroseconds(0, 1500); 
-   delay(10);
+   delay(3000);
 }
 
 void loop() {
-  pwm.writeMicroseconds(0, 1500); 
+  pwm.writeMicroseconds(0, 1900); 
   delay(3000); 
-  pwm.writeMicroseconds(0 ,1900); 
+  pwm.writeMicroseconds(0 ,1100); 
   delay(3000); 
 }
 
--- Servo_library_adjusted/Servo_library_adjusted.ino
+++ Servo_library_adjusted/Servo_library_adjusted.ino
@@ -3,15 +3,28 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  300 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX 450  // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  1200 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  1800 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 60 
+#define SERVO_FREQ 60 // Analog servos run at ~60 Hz updates
 
-uint8_t servonum = 0;
+int pot;
+int sec; 
+
+long timeThis, timeLast;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("8 channel Servo test!");
-
+ 
   pwm.begin();
+  pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz
+  pwm.setPWMFreq(SERVO_FREQ);
+  delay(2000);
+}
+
+void loop() {
+  timeThis = millis();
+  pot = analogRead(3);
+  sec = map(pot, 0, 1023, 600, 2200); 
+if (timeThis - timeLast > 1000) {
+  Serial.print(pot); Serial.print('\t'); Serial.println(sec); 
+}
+  pwm.writeMicroseconds(0, sec); 
+}
