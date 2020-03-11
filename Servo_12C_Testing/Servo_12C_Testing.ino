2b0d7780c0be696262507e0a51193d651f662881
Lucas <lchaves22@medford.k12.ma.us>
2020-03-11 11:25:46 -0400

    Update Servo_12C_Testing.ino

--- Servo_12C_Testing/Servo_12C_Testing.ino
+++ Servo_12C_Testing/Servo_12C_Testing.ino
@@ -1,12 +1,10 @@
+#include <Adafruit_LEDBackpack.h>
+
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
-#define SERVOMIN  300 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX 450  // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  1200 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  1800 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 60 // Analog servos run at ~60 Hz updates
 
 
